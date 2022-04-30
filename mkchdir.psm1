e188084fdb431fd7c5939e4f5eefd11c85784b6d
Paramon, Yevstigneyev <90145033+ParamonPlay2205@users.noreply.github.com>
2022-04-29 21:36:05 -0700

    Update mkchdir.psm1

--- mkchdir.psm1
+++ mkchdir.psm1
@@ -1,26 +1,28 @@
-# PowerShell function that will set the users location to the name of the new directory 
-function Set-NewLocation {
-    [CmdletBinding()]
-    # String paramter which is recived by the user, to 
-    param (
-        [Parameter()]
-        [string]
-        $NewDirectory
-    )
-    # If the user does not enter a name for the new directory, then it will write a warning to the user.
-    if ($NewDirectory -eq "")
-    {
-        Write-Warning "`nPlease enter a directory name!`n"
-    # If the user enters a name for the new dicretory, then it will create the new directory and set the users location to the new directory.
-    } else {
-        # Gets the current running directory
-        $rd = Get-Location
-        # Formats the user's location and the new directory into one string
-        $dir = "${rd}\${NewDirectory}"
-        
-        # Creates the new directory in the user's running directory and changes it to the new directory.
-        New-Item -Path . -ItemType Directory -Name $NewDirectory | Out-Null
-        Set-Location $dir
+<#
+Created by: Paramon Yevstigneyev
+#>
+# PowerShell Function that will change your local directory to new director(y/ies) that the user specifies.
+function Set-NewDirectoryLocation
+{
+    # User string parameter for creating new director(y/ies)
+    param ([string]$NewDirectoryName)
+    try {
+        if ($NewDirectoryName -eq "")
+        {
+            Write-Warning "`nPlease enter a directory name to create`n"
+        } else
+        {
+            # Gets the user's running directory
+            $rundir = Get-Location
+            # Format's the running directory with the new director(y/ies)
+            $dir = "${rundir}\${NewDirectoryName}"
+            # Creates the director(y/ies)
+            New-Item -Path . -ItemType Directory -Name $NewDirectoryName | Out-Null
+            # After the new director(y/ies) are made, it will set the user location to the new path.
+            Set-Location $dir
+        }
+    # If any error occurs, then it will write the error out into the console.
+    } catch {
+        Write-Error
     }
-    
 }
